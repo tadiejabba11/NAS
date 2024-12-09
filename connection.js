f7ebbe8feed8aa95410698b4b26943995842c672
tadiejabba11 <136700050+tadiejabba11@users.noreply.github.com>
2024-12-09 23:50:44 +0200

    Add files via upload

--- connection.js
+++ connection.js
@@ -1,29 +1,17 @@
 
+const mysql=require('mysql2');
 
 
 
-  
-  require('dotenv').config();
-  const mysql = require('mysql2');
-  
-  const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10
+const pool = mysql.createPool({
+    host: 'localhost', // Replace with your database host
+    user: 'root', // Replace with your database user
+    password: '', // Replace with your database password
+    database: 'NAS', // Replace with your database name
+    connectionLimit: 10 // Adjust connection limit as needed (default is 10)
   });
   
-  connection.connect((error) => {
-    if (error) {
-      console.error('Error connecting to the database:', error.stack);
-      return;
-    }
-    console.log('Connected to the database.');
-  });
-  
-  module.exports = connection;
-  
+
 
 
 
