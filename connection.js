093cef163fad4cea30cea85dd68545a3c830c867
tadiejabba11 <136700050+tadiejabba11@users.noreply.github.com>
2024-12-10 00:43:01 +0200

    Add files via upload

--- connection.js
+++ connection.js
@@ -4,11 +4,12 @@ const mysql=require('mysql2');
 
 
 const pool = mysql.createPool({
-    host: 'localhost', // Replace with your database host
+    host: '127.0.0.1', // Replace with your database host
     user: 'root', // Replace with your database user
     password: '', // Replace with your database password
     database: 'NAS', // Replace with your database name
-    connectionLimit: 10 // Adjust connection limit as needed (default is 10)
+    connectionLimit: 10, // Adjust connection limit as needed (default is 10)
+    port:3306
   });
   
 
